1d1e6ccd2f05b364770000814d388da1febe7a0c
hiroyukimaekawa-lang <hiroyuki.maekawa@crestix-inc.com>
2026-09-14 15:12:31 +0900

    Update shared GAS endpoint

--- data/config.ts
+++ data/config.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_GAS_URL =
-  'https://script.google.com/macros/s/AKfycbzrBTYf8Bwmjwbx5BOcqm1jwEzh-TJ0J3dPO-j99gXzy4SmexCE8uZhO6V1rB2mGLg9/exec';
+  'https://script.google.com/macros/s/AKfycbxoGBuRblAo7EYdij2fwr0yeHGkqZrlRfl2yOJ56hXX4wySn7EW4QwkyuREYyoL2jvn/exec';
 
 export const DEFAULT_GOOGLE_REVIEW_URL =
   'https://www.google.com/search?q=%E4%B8%89%E5%AE%AE%E8%83%83%E8%85%B8%E5%86%85%E7%A7%91&oq=%E4%B8%89%E5%AE%AE%E8%83%83%E8%85%B8%E5%86%85%E7%A7%91&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIKCAQQABiABBiiBDIGCAUQRRg90gEHNDQ1ajBqN6gCALACAA&sourceid=chrome&source=chrome.ob&ie=UTF-8#lrd=0x3538b71c0287047f:0xc6b54829783b8e48,3,,,,';
