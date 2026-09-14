c1536bede21897451195ddbd6b895593b4b342ad
hiroyukimaekawa-lang <hiroyuki.maekawa@crestix-inc.com>
2026-09-14 15:31:43 +0900

    Remove public GAS environment config

--- data/config.ts
+++ data/config.ts
@@ -1,6 +1,3 @@
-export const DEFAULT_GAS_URL =
-  'https://script.google.com/macros/s/AKfycbxoGBuRblAo7EYdij2fwr0yeHGkqZrlRfl2yOJ56hXX4wySn7EW4QwkyuREYyoL2jvn/exec';
-
 export const DEFAULT_GOOGLE_REVIEW_URL =
   'https://www.google.com/search?q=%E4%B8%89%E5%AE%AE%E8%83%83%E8%85%B8%E5%86%85%E7%A7%91&oq=%E4%B8%89%E5%AE%AE%E8%83%83%E8%85%B8%E5%86%85%E7%A7%91&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIKCAQQABiABBiiBDIGCAUQRRg90gEHNDQ1ajBqN6gCALACAA&sourceid=chrome&source=chrome.ob&ie=UTF-8#lrd=0x3538b71c0287047f:0xc6b54829783b8e48,3,,,,';
 
@@ -12,7 +9,6 @@ export const clinicConfig = {
   name: '三宮胃腸内科',
   industry: '内科・消化器内科',
   homepageUrl: 'https://sannomiya-clinic.com/',
-  gasUrl: DEFAULT_GAS_URL,
   googleReviewUrl: resolveGoogleReviewUrl(process.env.NEXT_PUBLIC_GOOGLE_REVIEW_URL),
 } as const;
 
